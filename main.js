c9dc5d6996c1983b57e07b7c5e3a7dcde385e518
Manikandan <manikandanid565@gmail.com>
2022-05-30 18:30:08 +0530

    Added Search Bar and filter button (UI)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './scss/main.scss'
 
-
+// Theme changer with local storage
 let themeChangerBtn = document.querySelector('[data-themeSwitch]')
 let container = document.body
 const dataTheme = localStorage.getItem('data-theme')
@@ -30,3 +30,11 @@ if (dataTheme === 'dark'){
 }
 
 
+// Options List toggle
+
+let optionsButton = document.querySelector('[data-optionsButton]')
+let optionsList = document.querySelector('[data-optionsList]')
+
+optionsButton.addEventListener('click', ()=>{
+    optionsList.classList.toggle('opened')
+})
