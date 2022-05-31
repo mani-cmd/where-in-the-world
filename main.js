d116ad8b995ef018d19003b2a8ba09eddd2381a7
Manikandan <manikandanid565@gmail.com>
2022-05-31 13:31:46 +0530

    Search and filter functinality completed

--- main.js
+++ main.js
@@ -1,120 +1,141 @@
-import './scss/main.scss'
-import axios from 'axios';
+import "./scss/main.scss";
+import axios from "axios";
+let url = `https://restcountries.com/v3.1/all`;
 
 // Theme changer with local storage
-let themeChangerBtn = document.querySelector('[data-themeSwitch]')
-let container = document.body
-const dataTheme = localStorage.getItem('data-theme')
+let themeChangerBtn = document.querySelector("[data-themeSwitch]");
+let container = document.body;
+const dataTheme = localStorage.getItem("data-theme");
 container.setAttribute("data-theme", "light");
 
-themeChangerBtn.addEventListener('click', ()=>{
-    
-    let dataAttribute = container.getAttribute('data-theme')
+themeChangerBtn.addEventListener("click", () => {
+    let dataAttribute = container.getAttribute("data-theme");
 
     if (dataAttribute === "light") {
         container.setAttribute("data-theme", "dark");
         localStorage.setItem("data-theme", "dark");
-  
     }
-    if(dataAttribute === "dark") {
+    if (dataAttribute === "dark") {
         container.setAttribute("data-theme", "light");
         localStorage.setItem("data-theme", "light");
-        
     }
-    
-})
+});
 
-if (dataTheme === 'dark'){
-    container.setAttribute("data-theme", "dark")
-}else if(dataTheme === "light"){
-    container.setAttribute("data-theme", "light")
+if (dataTheme === "dark") {
+    container.setAttribute("data-theme", "dark");
+} else if (dataTheme === "light") {
+    container.setAttribute("data-theme", "light");
 }
-
 
 // Options List toggle
 
-let optionsButton = document.querySelector('[data-optionsButton]')
-let optionsList = document.querySelector('[data-optionsList]')
+let FilterButton = document.querySelector("[data-optionsButton]");
+let FilterButtonText = document.querySelector("[data-optionsButton] > span");
+let optionsList = document.querySelector("[data-optionsList]");
+let optionButtons = document.querySelectorAll(
+    "[data-optionsList] > li > button"
+);
 
-optionsButton.addEventListener('click', ()=>{
-    optionsList.classList.toggle('opened')
-})
+FilterButton.addEventListener("click", () => {
+    optionsList.classList.toggle("opened");
+});
+
+console.log(optionButtons);
+
+optionButtons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        optionsList.classList.toggle("opened");
+
+        if(e.target.innerText === 'Reset'){
+            FilterButtonText.innerText = 'Filter by region'
+            axios({
+                method: "get",
+                url: url,
+            }).then((response) => {
+                populateData(response.data);
+            });
+        }else{
+            
+            FilterButtonText.innerText = e.target.innerText
+            
+            axios({
+                method: "get",
+                url: `https://restcountries.com/v3.1/region/${e.target.innerText}`,
+            }).then((response) => {
+                populateData(response.data);
+            });
+            
+        }
+    });
+});
 
 // Populate data
-function populateData(data){
-    let content = document.querySelector('[data-content]')
-    content.innerHTML = ''
-    console.log(data)
+function populateData(data) {
+    let content = document.querySelector("[data-content]");
+    content.innerHTML = "";
+    console.log(data);
 
-    for (let i = 0; i <= data.length -3 ; i++) {
-        let r = i + 3
-        let card = document.createElement("a")  
-        card.classList.add('card')
-        
-        let cardImgContainer = document.createElement("div")
-        cardImgContainer.classList.add('cardImage')
-        
-        let cardImg = document.createElement("img")
-        
-        let cardInfo = document.createElement('div')
-        cardInfo.classList.add('cardInformation')
+    for (let i = 0; i <= data.length; i++) {
+        let r = i;
+        let card = document.createElement("a");
+        card.classList.add("card");
 
-        let countryName = document.createElement('h2')
-        let Population = document.createElement('p')
-        let Region = document.createElement('p')
-        let Capital = document.createElement('p')
+        let cardImgContainer = document.createElement("div");
+        cardImgContainer.classList.add("cardImage");
 
+        let cardImg = document.createElement("img");
 
-        countryName.innerText = data[r].name.common
-        Population.innerHTML = `Population: <span>${data[r].population}</span>`
-        Region.innerHTML = `Region: <span>${data[r].region}</span>`
-        Capital.innerHTML = `Capital: <span>${data[r].capital[0]}</span>`
-        cardImg.src = `${data[r].flags.png}`
+        let cardInfo = document.createElement("div");
+        cardInfo.classList.add("cardInformation");
+
+        let countryName = document.createElement("h2");
+        let Population = document.createElement("p");
+        let Region = document.createElement("p");
+        let Capital = document.createElement("p");
+        let capitalName =
+            data[r].hasOwnProperty("capital") === true
+                ? data[r].capital[0]
+                : "none";
+
+        countryName.innerText = data[r].name.common;
+        Population.innerHTML = `Population: <span>${data[r].population}</span>`;
+        Region.innerHTML = `Region: <span>${data[r].region}</span>`;
+        Capital.innerHTML = `Capital: <span>${capitalName}</span>`;
+        cardImg.src = `${data[r].flags.png}`;
         // cardImgContainer.style = ` background-image: url('${data[r].flags.png}')`
-        cardImg.alt = data[r].name.common + '\'s flag'
+        cardImg.alt = data[r].name.common + "'s flag";
 
-        cardInfo.appendChild(countryName)
-        cardInfo.appendChild(Population)
-        cardInfo.appendChild(Region)
-        cardInfo.appendChild(Capital)
-        
-        cardImgContainer.appendChild(cardImg)
-        
-        card.appendChild(cardImgContainer)
-        card.appendChild(cardInfo)
-        
-        content.appendChild(card)
-        console.log(data[r].flags)
+        cardInfo.appendChild(countryName);
+        cardInfo.appendChild(Population);
+        cardInfo.appendChild(Region);
+        cardInfo.appendChild(Capital);
 
+        cardImgContainer.appendChild(cardImg);
+
+        card.appendChild(cardImgContainer);
+        card.appendChild(cardInfo);
+
+        content.appendChild(card);
     }
 }
 
 // getting request from api
 
-let url = `https://restcountries.com/v3.1/all`
 
 axios({
-    method: 'get',
+    method: "get",
     url: url,
-  }).then(
-        (response)  =>{
-        
-            populateData(response.data)
-      
-        }
-    );
+}).then((response) => {
+    populateData(response.data);
+});
 
+let inputBox = document.querySelector("[data-input]");
 
-/*******************************************************************************\
-<div class="card">
-     <div class="cardImage">
-        <img src="https://flagcdn.com/do.svg" alt="" />
-     </div>
-     <div class="cardInformation">
-         <h2>{Country Name}</h2>
-         <p>Population: <span>10</span></p>
-         <p>Region: <span>Fire Country</span></p>
-         <p>Capital: <span>Konoha</span></p>
-     </div>
-</div>
-\*******************************************************************************/
+inputBox.addEventListener("input", (e) => {
+    axios({
+        method: "get",
+        url: `https://restcountries.com/v3.1/name/${e.target.value}`,
+    }).then((response) => {
+        populateData(response.data);
+    });
+});
